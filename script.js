9416a9fd8eff2552aacb0e56c865bb332cc43925
NeilDatt89 <81631669+NeilDatt89@users.noreply.github.com>
2021-07-12 07:24:41 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function finishLoad(){
-    document.getElementById("loader").style.transition = "1s";
-    document.getElementById("loader").style.visibility="hidden";
-}
-setTimeout("finishLoad()", 5500);
